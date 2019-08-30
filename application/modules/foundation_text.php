953329785e957902e5eb674296e2de9d2e0953b5
Amanda <amanda.p.reyes@gmail.com>
2019-08-30 15:45:37 +0200

    Addded more styling to section

--- application/modules/foundation_text.php
+++ application/modules/foundation_text.php
@@ -6,9 +6,9 @@
 
 function module_foundation_text($module){
   ?>
-  <section id="foundation" class="section">
+  <section id="foundation" class="section section__foundation">
     <div class="container">
-    <div class="foundation-block">
+    <div class="foundation__block">
     <img class="foundation_img" src="<?php echo get_template_directory_uri(); ?>/images/foundation_icon.png" alt="Avicii foundation icon" width="250">
       <div class="foundation_text"><?= $module['foundation_text'] ?></div>
     </div>
