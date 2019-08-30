3b44940850b35c485b8c37081453944429baa21a
Amanda <amanda.p.reyes@gmail.com>
2019-08-30 13:59:56 +0200

    Easy smooth scrolling

---
+++ application/modules/textbox.php
@@ -0,0 +1,15 @@
+<?php
+
+
+
+/* TEXTBOX */
+
+function module_textbox($module){
+  ?>
+  <section class="section section__textbox">
+    <div class="container">
+      <div class="textbox-text"><?= $module['text'] ?></div>
+    </div>
+  </section>
+  <?php
+}
