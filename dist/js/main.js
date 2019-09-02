1f38eec55d5be661e53a873d092c39b733fbc713
Amanda <amanda.p.reyes@gmail.com>
2019-09-02 02:03:26 +0200

    Fixed smooth scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,99 +1,103 @@
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+$(document).ready(function() {
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
 
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-          // Prevent default anchor click behavior
-          event.preventDefault();
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-          // Store hash
-          var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-          // Using jQuery's animate() method to add smooth page scroll
-          $('html').animate({
-              scrollTop: $(hash).offset().top
-          }, 800, function() {
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
 
-              // Add hash (#) to URL when done scrolling (default click behavior)
-              window.location.hash = hash;
-          });
-      }
-  });
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            });
+        } // End if
+    });
+});
 
-  window.onscroll = function() { myFunction() };
 
-  var header = document.getElementById("myHeader");
-  var sticky = header.offsetTop;
+window.onscroll = function() { myFunction() };
 
-  function myFunction() {
-      if (window.pageYOffset > sticky) {
-          header.classList.add("sticky");
-      } else {
-          header.classList.remove("sticky");
-      }
-  }
-  //timer countdown
-  function getTimeRemaining(endtime) {
-      var t = Date.parse(endtime) - Date.parse(new Date());
-      var seconds = Math.floor((t / 1000) % 60);
-      var minutes = Math.floor((t / 1000 / 60) % 60);
-      var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-      var days = Math.floor(t / (1000 * 60 * 60 * 24));
-      return {
-          'total': t,
-          'days': days,
-          'hours': hours,
-          'minutes': minutes,
-          'seconds': seconds
-      };
-  }
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
 
-  function initializeClock(id, endtime) {
-      var clock = document.getElementById(id);
-      var daysSpan = clock.querySelector('.days');
-      var hoursSpan = clock.querySelector('.hours');
-      var minutesSpan = clock.querySelector('.minutes');
-      var secondsSpan = clock.querySelector('.seconds');
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+//timer countdown
+function getTimeRemaining(endtime) {
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
 
-      function updateClock() {
-          var t = getTimeRemaining(endtime);
+function initializeClock(id, endtime) {
+    var clock = document.getElementById(id);
+    var daysSpan = clock.querySelector('.days');
+    var hoursSpan = clock.querySelector('.hours');
+    var minutesSpan = clock.querySelector('.minutes');
+    var secondsSpan = clock.querySelector('.seconds');
 
-          daysSpan.innerHTML = t.days;
-          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+    function updateClock() {
+        var t = getTimeRemaining(endtime);
 
-          if (t.total <= 0) {
-              clearInterval(timeinterval);
-          }
-      }
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-      updateClock();
-      var timeinterval = setInterval(updateClock, 1000);
-  }
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+    }
 
-  var deadline = 'December 5 2019 16:00:00';
-  initializeClock('clockdiv', deadline);
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
 
-  //Ripple
-  var links = document.querySelectorAll('.ripplelink');
+var deadline = 'December 5 2019 16:00:00';
+initializeClock('clockdiv', deadline);
 
-  for (var i = 0, len = links.length; i < len; i++) {
-      links[i].addEventListener('click', function(e) {
-          var targetEl = e.target;
-          var inkEl = targetEl.querySelector('.ink');
+//Ripple
+var links = document.querySelectorAll('.ripplelink');
 
-          if (inkEl) {
-              inkEl.classList.remove('animate');
-          } else {
-              inkEl = document.createElement('span');
-              inkEl.classList.add('ink');
-              inkEl.style.width = inkEl.style.height = Math.max(targetEl.offsetWidth, targetEl.offsetHeight) + 'px';
-              targetEl.appendChild(inkEl);
-          }
+for (var i = 0, len = links.length; i < len; i++) {
+    links[i].addEventListener('click', function(e) {
+        var targetEl = e.target;
+        var inkEl = targetEl.querySelector('.ink');
 
-          inkEl.style.left = (e.offsetX - inkEl.offsetWidth / 2) + 'px';
-          inkEl.style.top = (e.offsetY - inkEl.offsetHeight / 2) + 'px';
-          inkEl.classList.add('animate');
-      }, false);
-  }
+        if (inkEl) {
+            inkEl.classList.remove('animate');
+        } else {
+            inkEl = document.createElement('span');
+            inkEl.classList.add('ink');
+            inkEl.style.width = inkEl.style.height = Math.max(targetEl.offsetWidth, targetEl.offsetHeight) + 'px';
+            targetEl.appendChild(inkEl);
+        }
+
+        inkEl.style.left = (e.offsetX - inkEl.offsetWidth / 2) + 'px';
+        inkEl.style.top = (e.offsetY - inkEl.offsetHeight / 2) + 'px';
+        inkEl.classList.add('animate');
+    }, false);
+}
